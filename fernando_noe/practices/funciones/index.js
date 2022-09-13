e0b2e066a360e1eebd0adc3fac86327a0b3b19b9
Fernando Noé <fernandomnoe024@gmail.com>
2022-09-13 20:42:25 -0300

    added function file in practice

---
+++ fernando_noe/practices/funciones/index.js
@@ -0,0 +1,17 @@
+/*
+()=>{};  Funcion Flecha "Anónima"
+ */
+
+/* 
+function nombre(){}; Función con entorno
+*/
+
+//Una funcion es una forma de declarar codigo en bloques
+//Una funcion puede recibir datos de entrada que son llamados "Parámetros"
+//Una funcion puede devolver datos de salida que son conocidos como resultado 
+
+function suma(a,b){
+ return a+b;
+}
+
+console.log(suma(3,2));
