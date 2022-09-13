94045c2d625e6a70208da15df5f7a1cd7866bb1e
p-glinka <glinkapg@hotmail.com>
2022-09-13 20:28:38 -0300

    añadido console.log

---
+++ paulo_glinka/practices/variables/index.js
@@ -0,0 +1,29 @@
+// comentario en una sola lines
+/* esto es un 
+comentario de varias 
+lineas */ 
+
+/**
+ * esta variable es declaradda golabalmete, usar var es una mala practica
+ */
+var miVariable; 
+
+/**
+ * esta variable es declarada solamente en un bloque de codigo
+ */
+
+let otraVariable =  "un nombre";
+
+/**
+ * Esta variable es declarada en un bloque de codigo pero
+ * no podra ser modificada
+ */
+const unaConstante = " Un valor";
+
+otraVariable = "otro nombre";
+
+
+
+console.log (otraVariable)
+
+
