c1b993b68de410ed3362726aeedde22d8f1a0945
conimartinez <89652610+conimartinez@users.noreply.github.com>
2022-09-13 20:28:49 -0300

    added console log in variables practice

---
+++ constanza_martinez/practices/variables/index.js
@@ -0,0 +1,18 @@
+/**
+ * Esta variable es global en toda la aplicación
+ */
+var miVariable;
+
+/**
+ * Esta variable es declarada solamente en un bloque de código
+ */
+let otraVariable;
+
+/**
+ * Esta variable es declarada solamente en un bloque de código, pero no podrá ser cambiada más adelante
+ */
+const unaConstante = 'un valor';
+
+otraVariable = 'otro nombre';
+
+console.log(otraVariable);
--- tasks/lesson_1/script.js
+++ tasks/lesson_1/script.js
@@ -1,1 +1,22 @@
-alert("Se debe crear la logica en el archivo script.js");
+// alert("Se debe crear la logica en el archivo script.js");
+
+    let coin1 = document.getElementById(coin_1).value;
+    let coin2 = document.getElementById(coin_2).value;
+    let coin3 = document.getElementById(coin_3).value;
+    let coin4 = document.getElementById(coin_4).value;
+    
+    let multiply1 = document.getElementById(multiplier_1).value;
+    let multiply2 = document.getElementById(multiplier_2).value;
+    let multiply3 = document.getElementById(multiplier_3).value;
+    let multiply4 = document.getElementById(multiplier_4).value;
+    
+    let multiplication1 = coin1 * multiply1;
+    let multiplication2 = coin2 * multiply2;
+    let multiplication3 = coin3 * multiply3;
+    let multiplication4 = coin4 * multiply4;
+
+    console.log(multiplication1);
+
+    let showResult = document.getElementById(calculate);
+
+    showResult.addEventListener('click', )
