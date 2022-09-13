32e542d4100e9023c2260046cb4ef7bc4d2afac0
juliandalessandro <julian.dalessandro.7@gmail.com>
2022-09-13 20:42:23 -0300

    added function file in practice

---
+++ julian_dalessandro/practices/funciones/index.js
@@ -0,0 +1,18 @@
+/*
+    ()=>{} -> Es una funcion anonima
+*/
+/*
+    function nombre(){} -> Es una funcion con entorno
+*/
+
+/*
+    Una funcion es una forma de declarar codigo en bloques
+    Puede recibir datos de entrada que son conocidos como parametros
+    Puede devolver datos de salida que son conocidos como resultados
+*/
+
+const suma = function(a, b){
+    return a + b;
+}
+
+console.log(suma(1, 2));
