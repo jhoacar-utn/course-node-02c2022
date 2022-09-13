1e5c0ffcb8f7b25e75bac13546f0d36cac6532d1
juliandalessandro <julian.dalessandro.7@gmail.com>
2022-09-13 20:28:46 -0300

    added console.log in variables practice

---
+++ julian_dalessandro/practices/variables/index.js
@@ -0,0 +1,28 @@
+//Esto es un comentario de una linea
+
+/*
+    Esto es un comentario
+    con mas lineas
+*/
+
+/**
+*   Esta variable es declarada globalmente
+*/
+
+var miVariable;
+
+/**
+*   Esta variable es declarada solamente en un bloque de codigo
+*/
+
+let otraVariable = "un nombre";
+
+/**
+*   Esta variable es declarada solamente en un bloque de codigo pero no podra ser modificada luego
+*/
+
+const unaConstante = "un valor";
+
+otraVariable = "otro nombre";
+
+console.log(otraVariable);
