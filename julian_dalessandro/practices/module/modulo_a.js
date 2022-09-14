2610902e26c7699a791189927c5dbb6a06c2cca5
juliandalessandro <julian.dalessandro.7@gmail.com>
2022-09-13 21:53:40 -0300

    added module file in practice

---
+++ julian_dalessandro/practices/module/modulo_a.js
@@ -0,0 +1,5 @@
+var variableA = "Variable A";
+
+console.log("Estoy en el modulo A")
+
+module.exports = variableA;
---
+++ julian_dalessandro/practices/module/modulo_b.js
@@ -0,0 +1,5 @@
+var variableB = "Variable B";
+
+console.log("Estoy en el modulo B")
+
+module.exports = variableB;
