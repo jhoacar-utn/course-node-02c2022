8f3476c63c01ac16d1061c1c069c9e31edafdf36
jhoacar <jhoacar.utn@gmail.com>
2022-09-16 19:53:50 -0300

    added helper in information about arrow functions

--- practices/arrow_functions/index.js
+++ practices/arrow_functions/index.js
@@ -4,15 +4,23 @@
 /*
     function nombre(){} -> Es una funcion con entorno -> Funcion normal
 */
-
+/**
+ * Esta declaracion es anonima, es decir, no estara vinculada
+ * de ninguna manera de los atributos del objeto al que pertenece
+ */
 const funcion_anonima = ()=>{
     console.log("Soy una funcion anonima")
     console.log(this)
 }
-
+/**
+ * Esta declaracion acepta el entorno en el que se haya declarado
+ * basicamente seria poder acceder a todo los atributos del objeto
+ * al que pertenece
+ */
 const funcion_normal = function(){
     console.log("Soy una funcion normal")
     console.log(this)
+    console.log(this.nombre)
 }
 
 const estudiante = {
