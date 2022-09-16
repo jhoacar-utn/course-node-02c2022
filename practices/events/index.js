a290ad6b19eccd4cf37b30a11c80a17a4ccb5132
jhoacar <jhoacar.utn@gmail.com>
2022-09-16 20:07:44 -0300

    added event practice

---
+++ practices/events/index.js
@@ -0,0 +1,40 @@
+/**
+ * Podriamos relacionar los eventos y la programacion a partir de eventos
+ * como aquellas funciones que se ejecutaran luego de una accion
+ * - Podemos relacionarlo con el concepto de gancho (hook)
+ */
+
+const controladorDeEventos = {
+    eventos: {},
+    registrar: function (name, callback) {
+        this.eventos[name] = callback;
+        console.log("Eventos registrados: ", this.eventos);
+    },
+    emitir: function (name) {
+
+        const evento = this.eventos[name];
+
+        if (typeof evento !== "function") {
+            console.log("El evento registrado no es una funcion");
+            return;
+        }
+
+        if (name === "click") {
+            evento("Se ha dado click")
+        }
+        if (name === "keypress") {
+            evento("Se ha presionado una tecla")
+        }
+    }
+};
+
+
+const mostrar = function (resultado) {
+    console.log(resultado);
+}
+
+controladorDeEventos.registrar('click', mostrar) // 'on'
+controladorDeEventos.registrar('keypress', mostrar) // 'on'
+
+controladorDeEventos.emitir('click') // 'emit'
+controladorDeEventos.emitir('keypress')
