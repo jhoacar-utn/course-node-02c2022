b8e60db4ee3cd6f225da082cca2342784e6f98b7
jhoacar <jhoacar.utn@gmail.com>
2022-09-16 21:32:14 -0300

    added example for http module

---
+++ practices/http-module/index.js
@@ -0,0 +1,25 @@
+const http = require("http")
+
+/**
+ * En el modulo http, la callback para manejar
+ * la conexion recibe dos parametros:
+ * - El primer parametro es un objeto con la informacion de la peticion del cliente
+ *  es conocida como 'request'
+ * - El segundo parametro es un objeto con metodos para enviar la respuesta
+ *  es conocida como 'response'
+ */
+function handleConnection(request, response){
+    console.log("Conexion establecida")
+
+    response.write("Hola desde el servidor")
+    response.end()
+}
+
+const server = http.createServer(handleConnection);
+
+
+function handleStartServer(){
+    console.log("Servidor funcionando correctamente")
+}
+
+server.listen(8888,handleStartServer)
