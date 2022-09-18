4b4a2602bdfdd6e3a5ee5fcd825fdf579b856b08
unknown <jonatangobbo1@gmail.com>
2022-09-17 22:03:56 -0300

    added console log in variables practice

---
+++ jonatan_gobbo/practices/variables/index.js
@@ -0,0 +1,24 @@
+// comentario de una linea  
+/* 
+Esto es un comentario
+de varias lineas*/
+
+/**
+ * Esta variable es declarada globalmente
+ */
+var miVariable;
+
+/**
+ * Esta variable es declarada solamente en un bloque de codigo
+ */
+
+let otraVariable;
+
+/**
+ * Esta variable es declarada solamente en un bloque de codigo pero solo en modo lectura
+ */
+const unaConstante = "un valor";
+
+otraVariable = "otro nombre";
+
+console.log(otraVariable);
