c7d3df723989b0c99f74387dbc8f743ec05a8af4
jhoacar <jhoacar.utn@gmail.com>
2022-09-20 20:43:25 -0300

    added loop documentation and link for event loop in javascript

---
+++ practices/fundamentals/basic/loops/index.js
@@ -0,0 +1,37 @@
+/**
+ * Los bucles son sentencias de codigo que se repetiran en cantidades definidas
+ * o indefinidas
+ * 
+ * - for, while, do while
+ */
+
+
+
+// El ciclo for se usa para ejecutar sentencias de codigo una cantidad definida de veces
+
+const notas = [5,8,4,7,6,3,2]
+
+let maxNota = notas[0]
+let materiaDeLaNota = 0
+
+for (let materia = 0; materia < notas.length; materia++) {
+    // console.log(notas[materia])
+    if(notas[materia] > maxNota){
+        maxNota = notas[materia]
+        materiaDeLaNota = materia
+    }
+}
+
+console.log(maxNota)
+console.log(materiaDeLaNota)
+
+// El ciclo while se usa para ejecutar sentencias de codigo una cantidad indefinida de veces
+
+const juego = {
+    state : "Jugando"
+}
+
+while(juego.state === "Jugando"){
+    console.log("Estoy jugando")
+    juego.state="Terminado"    
+}
