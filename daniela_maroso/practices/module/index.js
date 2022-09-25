7474663ab306515344aaddaca0636b66efa64b35
daninanni <daniailin@hotmail.com>
2022-09-25 15:35:33 -0300

    modules

---
+++ daniela_maroso/practices/module/index.js
@@ -0,0 +1,9 @@
+
+
+let otroNombre = require("./modulo_a")
+
+let variableB = require("./modulo_b")
+
+console.log(otroNombre)
+
+console.log(variableB)
---
+++ daniela_maroso/practices/module/modulo_a.js
@@ -0,0 +1,7 @@
+
+
+let variableA = "variable A";
+
+console.log("Estoy en el modulo A")
+
+module.exports = variableA
