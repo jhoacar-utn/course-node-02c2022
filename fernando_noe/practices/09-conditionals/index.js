5a895f0940960f5ae2f8c68720b173f793459d2c
Fernando Noé <fernandomnoe024@gmail.com>
2022-09-25 15:43:52 -0300

    added 09-conditionals file in practices

---
+++ fernando_noe/practices/09-conditionals/index.js
@@ -0,0 +1,43 @@
+/**
+ * Los condicionales son Bloques de código que se ejecutan a partir de una condición si es verdadera o falsa.
+ * -if, else, switch, ternario
+ * - && (AND), || (OR), !(NOT)
+ */
+
+
+/*Uso de IF*/
+
+const MAX_AGE_DISCO = 18;
+
+function goToDisco(){
+    console.log("Estoy en la Disco");
+}
+
+const persona = {
+    age: "17",
+    access: true,
+}
+
+if(persona.age >= MAX_AGE_DISCO){
+   goToDisco();
+}else if(persona.access){
+    console.log("Tiene acceso VIP");
+}else{
+    console.log("No puede pasar");
+}
+
+
+/*Uso de SWICH*/
+
+persona.cash = 12;
+
+switch(persona.cash){
+    case 4: 
+        console.log("puede comprar mandarinas");
+        break;
+    case 7:
+        console.log("puede comprar tomates");
+        break;
+    default:
+        console.log("puede comprar mandarinas y tomates");
+}
