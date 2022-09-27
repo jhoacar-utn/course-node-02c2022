ad63d2c514cb448074779ef074be4b73e8ab5db1
Fernando Noé <fernandomnoe024@gmail.com>
2022-09-26 21:20:52 -0300

    update 14-spread-operator file

--- fernando_noe/practices/14-spread-operator/index.js
+++ fernando_noe/practices/14-spread-operator/index.js
@@ -14,3 +14,28 @@ function suma(...nums){
 }
 
 console.log(suma(1,2,5,7));
+
+//Otro uso del Spread Operator con los Objetos
+console.log("---------------------------------");
+
+const Estudiante = {
+    name: "Defecto",
+    study: function() {
+        console.log(this.name + " está estudiando")
+    }
+};
+
+const sofia = {...Estudiante, name: "Sofia"};
+console.log(sofia);
+sofia.study();
+
+console.log("---------------------------------");
+//Otro uso del Spread Operator puede ser con los arrays
+
+const notas = [1,5,4,3];
+
+console.log(notas);
+
+const otrasNotas = [...notas, 9];
+
+console.log(otrasNotas);
