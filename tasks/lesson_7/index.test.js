0e4c70095344ebe691c919bd0dfd66ebabef3da8
jhoacar <carrerojhoan@gmail.com>
2022-09-29 19:34:44 -0300

    added solution to task lesson_7 and added new task lesson_8

--- tasks/lesson_7/index.test.js
+++ tasks/lesson_7/index.test.js
@@ -12,15 +12,15 @@ describe("Operaciones aritmeticas", () => {
             expect(sum("1", "2")).toBe(3);
         })
 
-        test.skip("Deberia arrojar una excepcion si no cumple que es un numero sus parametros", () => {
+        test("Deberia arrojar una excepcion si no cumple que es un numero sus parametros", () => {
             expect(() => sum("Hola", "2")).toThrow(new Error("Los parametros deben ser numeros"));
         })
 
-        test.skip("Deberia poder sumar tres o mas parametros", () => {
+        test("Deberia poder sumar tres o mas parametros", () => {
             expect(sum(1, 3, "6")).toBe(10);
         })
 
-        test.skip("Deberia sumar cadenas de texto o numeros con decimales", () => {
+        test("Deberia sumar cadenas de texto o numeros con decimales", () => {
             expect(sum(1, "2", 5.3)).toBe(8.3);
         })
     })
