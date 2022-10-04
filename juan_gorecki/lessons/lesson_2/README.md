8d68bd339d622f1743dffc626341f90c123a11e4
juan gore <juangorecki@gmail.com>
2022-10-04 18:06:40 -0300

    added console log in variables practices

---
+++ juan_gorecki/lessons/lesson_2/README.md
@@ -0,0 +1,5 @@
+# Contenidos vistos en clase:
+
+* Instalacion de Docker
+* Al usar en la terminal el `.` nos estamos refiriendo a la carpeta actual de trabajo
+* [Sockets](./sockets.md)
---
+++ juan_gorecki/practices/variables/index.js
@@ -0,0 +1,18 @@
+/**
+ * esta variable es declarada globamlmente en toda la aplicacion
+ */
+
+var miVariable; 
+/**
+ * esta variable es declarada solamente en un bloque de codigo
+ */
+let otraVariable = "un nombre";
+/**
+ * esta variable es declarada solamente en un bloque de codigo pero no podra ser cambiada mas adelante
+ */
+
+const unaConstante = "un valor";
+
+otraVariable = "otro nombre";
+
+console.log(otraVariable)
