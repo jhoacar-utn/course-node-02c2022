9a1fcb5ab92b784c9ec0882c22c726bfca23a2c4
jhoacar <jhoacar.utn@gmail.com>
2022-10-14 21:43:17 -0300

    added structure MVC in todo app

--- practices/to-do-app/index.js
+++ practices/to-do-app/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 
 const app = require("./server");
+const connection = require("./database");
 
 const port = process.env.PORT || 8888;
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await connection();
     console.log(`Server listening on http://localhost:${port}`);
 })
