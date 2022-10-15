1b9354265f61c825dcbdf5ee9f2f1e0141e8d152
jhoacar <jhoacar.utn@gmail.com>
2022-10-14 21:58:27 -0300

    added update and delete logic in controllers

--- practices/to-do-app/controllers/to-do.js
+++ practices/to-do-app/controllers/to-do.js
@@ -3,19 +3,23 @@ const ToDo = require("../models/to-do");
 module.exports.index = async (req, res) => {
     console.log("Index Function");
     const toDos = await ToDo.find();
-    response.send(toDos);
+    res.send(toDos);
 }
 
 module.exports.create = async (req, res) => {
     console.log("Create Function");
     const toDo = await ToDo.create(req.body);
-    response.send(toDo);
+    res.send(toDo);
 }
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
     console.log("Update Function");
+    await ToDo.updateOne({ title: req.query.title }, req.body);
+    res.send("Updated");
 }
 
-module.exports.remove = (req, res) => {
+module.exports.remove = async (req, res) => {
     console.log("Remove Function");
+    await ToDo.deleteOne({ title: req.query.title });
+    res.send("Removed");
 }
