fcc5e7b6dba62ec02e04690f378bf3b67a9c8e30
jhoacar <jhoacar.utn@gmail.com>
2022-10-18 20:25:46 -0300

    added logic in controllers

--- practices/to-do-app/controllers/priority.js
+++ practices/to-do-app/controllers/priority.js
@@ -1,12 +1,30 @@
+const ToDo = require("../models/to-do");
+
 module.exports.priority = async function (req, res) {
 
     console.log("Priority Function");
-    
-    res.json({
-        result: {
-            title: "Ejemplo",
-            text: "texto de ejemplo",
-            priority: 4
-        }
-    })
+
+    const { id } = req.params;
+
+    try {
+
+        const toDo = await ToDo.findByIdAndUpdate(id, {
+            priority: {
+                $inc: 1
+            }
+        })
+
+        res.json({
+            result: toDo
+        })
+
+    } catch (error) {
+        res.json({
+            errors: [
+                {
+                    message: error.message
+                }
+            ]
+        })
+    }
 }
