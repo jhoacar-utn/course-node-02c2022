a8bc48cbfaca157f4903bbbcca2185ae388fabfd
conimartinez <89652610+conimartinez@users.noreply.github.com>
2022-10-20 16:35:32 -0300

    feat: added auth model

---
+++ practices/to-do-app/models/auth.js
@@ -0,0 +1,10 @@
+
+const authSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: Number,
+});
+
+const auth = mongoose.model('auth', authSchema);
+
+module.exports = auth;
