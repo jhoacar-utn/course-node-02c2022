d615cf23aae6e7d838da7ab8f9725f30e45d23af
juan gore <juangorecki@gmail.com>
2022-10-21 13:54:35 -0300

    añadido function file practice

---
+++ juan_gorecki/practices/funciones/index.js
@@ -0,0 +1,21 @@
+/* 
+()=>{} funcion anonima
+*/
+
+/* 
+function nombre() funcion con entorno
+ */
+
+
+
+/* una funcion es una forma de declarar codigo en bloques}
+una funcion ouede recibir datos de entrada que son conocidos como parametros
+una funcion puede devolver datos de dslaida que son conocidos como resultado
+*/
+
+const suma = function(a,b){
+    return a+b;
+}
+
+console.log(suma(1,2))
+
