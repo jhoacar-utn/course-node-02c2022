25b16f2f46c5a42a23c39eb6344a70db12b95336
jhoacar <jhoacar.utn@gmail.com>
2022-10-21 19:29:23 -0300

    added client file for directory in frontend

--- practices/to-do-app/server/src/routes/web/index.js
+++ practices/to-do-app/server/src/routes/web/index.js
@@ -1,10 +1,9 @@
-const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
-const client = path.resolve(__dirname+"/../../client");
-console.log(client);
+const client = require("../../client");
 
 router.use(express.static(client));
 
