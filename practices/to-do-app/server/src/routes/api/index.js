83bf7ca8cb114ff515fc446d60d7c4dbbd5bf784
jhoacar <jhoacar.utn@gmail.com>
2022-10-21 19:44:37 -0300

    added cors in the server

--- practices/to-do-app/server/src/routes/api/index.js
+++ practices/to-do-app/server/src/routes/api/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 
 const router = express.Router();
+
+router.use(cors())
 
 router.use("/to-do", require("./to-do"));
 router.use("/priority", require("./priority"));
