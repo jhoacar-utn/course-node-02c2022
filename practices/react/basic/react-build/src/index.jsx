1a15a5f3554236e2d3ab42a43abda2548e461e94
jhoacar <jhoacar.utn@gmail.com>
2022-10-28 21:44:43 -0300

    added components in other files

--- practices/react/basic/react-build/src/index.jsx
+++ practices/react/basic/react-build/src/index.jsx
@@ -1,35 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function Header() {
-  return (
-    <header>
-      Cabecera
-    </header>
-  );
-}
-
-function Main() {
-  return (
-    <main>
-      Principal
-    </main>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>
-      Pie de Pagina
-    </footer>
-  );
-}
+import Cabecera from './header';
+import { Main as Principal } from './main';
+import { Footer } from './footer';
 
 function Body() {
   return (
     <div>
-      <Header />
-      <Main />
+      <Cabecera />
+      <Principal />
       <Footer />
     </div>
   );
