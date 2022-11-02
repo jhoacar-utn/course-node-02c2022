62bfbd5105ff2841cd6291dd609c812b8ec6589b
jhoacar <jhoacar.utn@gmail.com>
2022-11-01 21:24:12 -0300

    added useState hook

--- practices/react/vite-project/src/components/contador/Funcion.jsx
+++ practices/react/vite-project/src/components/contador/Funcion.jsx
@@ -1,20 +1,31 @@
 /* eslint-disable no-alert */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import { useState } from 'react';
+
 /* eslint-disable react/jsx-one-expression-per-line */
 function Contador() {
-  let contador = 0;
+  const [state, setState] = useState({ contador: 0 });
+  // const [otro, setOtro] = useState('Hola');
+  const [active, setActive] = useState(false);
+
   return (
     <div
       style={{
         padding: '1rem', border: 'solid red 1px', cursor: 'pointer', margin: '1rem',
       }}
       onClick={() => {
-        alert('Haciendo click en componente de funcion');
-        contador++;
+        // alert('Haciendo click en componente de funcion');
+        setState({
+          contador: state.contador + 1,
+        });
+        setActive(!active);
       }}
     >
-      Cantidad (Funcion) - {contador}
+      Cantidad (Funcion) - {state.contador}
+      {' '}
+      {active.toString()}
     </div>
   );
 }
