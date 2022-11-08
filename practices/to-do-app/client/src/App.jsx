8ab37a0f0a9fdd1188c4deb0f6924d9249c74eae
jhoacar <jhoacar.utn@gmail.com>
2022-11-08 19:47:21 -0300

    added scripts in global package.json and lazy loading for the pages

--- practices/to-do-app/client/src/App.jsx
+++ practices/to-do-app/client/src/App.jsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 
 function App() {
   return (
-    <RouterProvider router={router} />
+    <Suspense fallback={<h1>Cargando</h1>}>
+      <RouterProvider router={router} />
+    </Suspense>
   );
 }
 
--- practices/to-do-app/client/src/routes/index.jsx
+++ practices/to-do-app/client/src/routes/index.jsx
@@ -1,8 +1,36 @@
+import { lazy } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import Home from '../components/pages/home';
-import NotFound from '../components/pages/notfound';
-import ToDo from '../components/pages/todo';
-import ToDos from '../components/pages/todos';
+/**
+ * Uso de lazy loading (carga perezosa)
+ *
+ * El lazy loading es la sectorizacion de archivos
+ * para una carga independiente en las vistas,
+ * esto quiere decir, que no le mandaremos al
+ * usuario todo el contenido, sino solo aquellas
+ * partes que el requiera
+ *
+ * Para hacerlo se hace uso de la funcion lazy()
+ * y el componente <Suspense>, esto funciona de la
+ * siguiente manera:
+ *
+ *  - La funcion lazy() recibe como primer parametro,
+ *    una callback con que resolvera la promesa con la
+ *    importacion del recurso
+ *
+ *  - El componente <Suspense /> es el encargado
+ *    de procesar todas estas importaciones dinamicas
+ *    habilitando una 'fallback' que es un componente
+ *    que sera mostrado mientras se esta cargado dicho
+ *    recurso
+ */
+// import Home from '../components/pages/home';
+// import NotFound from '../components/pages/notfound';
+// import ToDo from '../components/pages/todo';
+// import ToDos from '../components/pages/todos';
+const Home = lazy(() => import('../components/pages/home'));
+const NotFound = lazy(() => import('../components/pages/notfound'));
+const ToDo = lazy(() => import('../components/pages/todo'));
+const ToDos = lazy(() => import('../components/pages/todos'));
 
 const router = createBrowserRouter([
   {
