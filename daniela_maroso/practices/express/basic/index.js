ff5388b5c8258a4ba713df8424c1399be6c5b1b5
daninanni <daniailin@hotmail.com>
2022-11-22 15:11:58 -0300

    express

--- daniela_maroso/practices/express/basic/index.js
+++ daniela_maroso/practices/express/basic/index.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require("express");             //la inicializacion// 
 
 const app = express();
 
--- daniela_maroso/practices/express/basic/server.js
+++ daniela_maroso/practices/express/basic/server.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express")           //la configuracion//
 
 const app = express()
 
