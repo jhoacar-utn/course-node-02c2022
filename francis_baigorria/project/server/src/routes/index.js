3161d2a5ac4f53d0de501f9cf9c0907bca78f771
FrancisCB92 <104937795+FrancisCB92@users.noreply.github.com>
2022-12-06 23:22:49 -0300

    votes route

--- francis_baigorria/project/server/src/routes/index.js
+++ francis_baigorria/project/server/src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const { index, show, vote } = require('../constrollers/emojis');
 
 router.get('/api/v1/emojis/:id', show);
-router.post('/api/v1/votes', vote);
+router.post('/api/v1/emojis/votes', vote);
 router.get('/api/v1/emojis', index);
 
 module.exports = router;
